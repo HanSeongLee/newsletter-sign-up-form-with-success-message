066d9fda67db22cd41c2e0631359cd55b272b37e
HanSeongLee <jkhanseong@gmail.com>
2023-10-27 17:34:47 +0900

    feat: add Roboto font

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google';
+
+const robotoFont = Roboto({
+  weight: ['400', '700'],
+  display: 'swap',
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <style jsx global>{`
+          :root {
+            --rotobo-font: ${robotoFont.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
