9abb2c58898c757435dadd51b6c22221ae3db2e9
HanSeongLee <jkhanseong@gmail.com>
2023-11-05 20:26:54 +0900

    feat: add 'icon' property to Form component

--- components/forms/Form/index.tsx
+++ components/forms/Form/index.tsx
@@ -1,22 +1,24 @@
-import React, { FormHTMLAttributes } from 'react';
+import React, { FormHTMLAttributes, ReactNode } from 'react';
 import styles from './style.module.scss';
 import cn from 'classnames';
 import { Paragraph, Title } from 'components/commons/Typography';
 
 interface IProps extends FormHTMLAttributes<HTMLFormElement> {
+    icon?: ReactNode;
     title: string;
     description: string | React.ReactNode;
     extra?: React.ReactNode;
 }
 
 const Form: React.FC<IProps> = ({
-                                    title, description, extra, className,
+                                    icon, title, description, extra, className,
                                     children, ...props
                                 }) => {
     return (
         <form className={cn(styles.form, className)}
               {...props}
         >
+            {icon}
             <div className={styles.head}>
                 <Title level={2}>
                     {title}
