ebb4258f9e968f6903d8aaa05334bb3f8fb4de18
HanSeongLee <jkhanseong@gmail.com>
2023-11-10 18:01:28 +0900

    feat: update home page

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
+import NewsletterSignUpContainer from 'containers/NewsletterSignUpContainer';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
+    return (
+        <>
+            <main className={styles.main}>
+                <div className={styles.container}>
+                    <NewsletterSignUpContainer />
+                </div>
+            </main>
+        </>
+    );
 }
 
-export default Home
+export default Home;
